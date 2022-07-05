30f5a146c62b21542b341feb1fcc7e925a9e404c
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-07-05 14:05:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Intro from './components/intro/Intro';
+import Skills from './components/skills/Skills';
+import Contact from './components/contact/Contact';
+import Projects from './components/projects/Projects';
 
 function App() {
+  var preloader = document.getElementById('loader');
+  function preLoaderHandler(){
+      preloader.style.display = 'none';
+  }
   return (
-    <div className="App">
-      <Navbar />
-      <Intro />
-    </div>
+      <div className="App">
+        {preLoaderHandler()}
+        <Navbar />
+        <Intro />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
   );
 }
 
