3d8123b4cd32f17b60b20194a4986ed5b6d1079b
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-07-05 14:11:13 -0400

    Create Contact.js

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,20 @@
+import "./Contact.css"
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className="contact" id="contact">
+        <div className="heading">
+          <h1>Contact me</h1>
+        </div>
+        <div className="links">
+          <h4>G-mail:   nadirkarimov76@gmail.com</h4>
+          <h4>LinkedIn:   https://www.linkedin.com/in/nadir-karimov-8a28a2242</h4>
+          <h4>GitHub:   https://github.com/Nadir-Enthusiast</h4>
+          <h4>Telegram:   @userNadir</h4>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
