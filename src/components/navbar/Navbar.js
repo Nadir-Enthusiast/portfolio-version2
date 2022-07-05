b18fc7e73619aaaa83cc0456003f0f319971055c
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-07-05 14:08:54 -0400

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,15 +2,10 @@ import "./Navbar.css"
 import React from 'react'
 
 function Navbar() {
-
-  const clickNav = (event) => {
-    console.log("ok")
-  }
-
   return (
     <div className="navbar">
         <div className="nav-container">
-            <a href="#about" onClick={(event) => clickNav(event)}>About me</a>
+            <a href="#intro">About me</a>
             <a href="#skills">Skills</a>
             <a href="#projects">Projects</a>
             <a href="#contact">Contact me</a>
